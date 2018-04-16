48731896cc21f57b3370887dd7625c6f4305bd4e
zizoua5000 <zizoua5000@gmail.com>
2018-04-16 16:09:36 +0400

    update lesson1

--- lesson1/task1/index.php
+++ lesson1/task1/index.php
@@ -1,115 +1,58 @@
-<h2>Код на html 1</h2>
-<? echo "<h1>Welcome!!!</h1>";
-   echo (9>7); 	
-?>
-<h2>Код на html 2</h2>
-
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <?
-//Математические операторы
-/*
-$x = date('d');
+    $title = "First Lesson";
+    $h1 = "minimalistica";
+    $year = date("Y");
+?>
 
-$x = $x+1;
-$x++;//инкремент
-$x+=1;
-++$x;
-$x--;//декремент
-
-$x*=10;
-$x= $x*10;
-
-$x=10 % 3;//1
-
-//Постфиксная форма инкремента
-$a = 10;
-$b = $a++;
-//echo "a=$a<br>b=$b";
-
-//Префиксная форма инкремента
-$a = 10;
-$b = ++$a;
-echo "a=".$a."<br>b=".$b;
-
-$test = "<br>Проект \"САОC\" - самый успешный проект компании";
-echo $test;
-
-*/
-//Логические операторы
-
-$x = true;//1
-$y = false;//0 
-/*
-x  y  AND(&&)   OR(||)   XOR(^^)
-1  1    1         1        0 
-1  0    0         1        1
-0  1    0         1        1
-0  0    0         0        0
-*/
-
-//Дома прокомментировать и объяснить почему false!
-//$z = !($x && !$y) || (!$x||!$y);
-//$z1 = !($x && !$y);
-//$z2 = (!$x||!$y);
-
-//echo $z;
-
-//Приведение типов данных
-
-$x = 25.235423;//тип double
-$x = (int)$x;
-//echo $x;
-
-//Неявное приведение к типу данных
-
-$a = 10;
-$b = $a + "5";
-//echo $b;
-
-//echo (10==="10");
-
-/*
-$test = 1;
-unset($test);
-echo $test;
-*/
-
-$text = "Сегодня ";
-$text.="выходной день";
-$text.=" суббота";
-
-//echo $text;
-
-//Математические функции
-
-echo rand(1,10);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
+	<link rel="stylesheet" href="css/main.css" type="text/css" />
+	<title><? echo $title ?></title>
+</head>
+<body>
+	<div id="content">
+		<h1><? echo $h1 ?></h1>
+		
+		<ul id="menu">
+			<li><a href="#">home</a></li>
+			<li><a href="#">archive</a></li>
+			<li><a href="#">contact</a></li>
+		</ul>
+	
+		<div class="post">
+			<div class="details">
+				<h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
+				<p class="info">posted 3 hours ago in <? echo $year ?> <a href="#">general</a></p> 
+			
+			</div>
+			<div class="body">
+				<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>
+			</div>
+			<div class="x"></div>
+		</div>
+		
+		<div class="col">
+			<h3><a href="#">Ut enim risus rhoncus</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col">
+			<h3><a href="#">Maecenas iaculis leo</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		<div class="col last">
+			<h3><a href="#">Quisque consectetur odio</a></h3>
+			<p>Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at.</p>
+			<p>&not; <a href="#">read more</a></p>
+		</div>
+		
+		<div id="footer">
+			<p>Copyright &copy; <em>minimalistica</em> &middot; Design: Luka Cvrk, <a href="http://www.solucija.com/" title="Free CSS Templates">Solucija</a></p>
+		</div>	
+	</div>
+</body>
+</html>
