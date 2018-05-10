8315310cd5357b03ce3f231bde0cf90d6d643c68
zizoua5000 <zizoua5000@gmail.com>
2018-05-10 17:43:07 +0400

    update

--- lesson6/admin.php
+++ lesson6/admin.php
@@ -17,6 +17,9 @@
 
     <!-- Waves Effect Css -->
     <link href="plugins/node-waves/waves.css" rel="stylesheet" />
+    
+    <!-- JQuery DataTable Css -->
+    <link href="plugins/jquery-datatable/skin/bootstrap/css/dataTables.bootstrap.css" rel="stylesheet">
 
     <!-- Animation Css -->
     <link href="plugins/animate-css/animate.css" rel="stylesheet" />
@@ -41,81 +44,87 @@
     $currentPage= "Items";
     include "header.php"; 
     include "sidemenu.php";
+    
     ?>
     
     <section class="content">
+        <div class="container-fluid">
          
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                <div class="card">
-                    <div class="header">
-                        <h2>ADD/UPDATE Items</h2>
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="card">
+                        <div class="header">
+                            <h2>ADD/UPDATE Items</h2>
 
-                        <div class="body">
-                            <form id="form_advanced_validation" class="js-ajax-php-json" action="#" method="POST"  enctype="multipart/form-data" novalidate="novalidate">
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <input type="text" class="form-control" name="name" required aria-required="true" id="item_n">
-                                        <label class="form-label">Item Name</label>
-                                    </div>
-                                </div>
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <textarea name="description_s" cols="30" rows="5" class="form-control no-resize" required aria-required="true"></textarea>
-                                        <label class="form-label">Short Description</label>
-                                    </div>
-                                </div>
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <textarea name="description_f" cols="30" rows="5" class="form-control" required aria-required="true"></textarea>
-                                        <label class="form-label">Full Description</label>
-                                    </div>
-                                </div>
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <input type="number" class="form-control" name="warehouse" required aria-required="true">
-                                        <label class="form-label">Quantity available in warehouse</label>
-                                    </div>
-                                    <div class="help-info">Numbers only</div>
-                                </div>
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <input type="number" class="form-control" name="price" required aria-required="true">
-                                        <label class="form-label">Price</label>
-                                    </div>
-                                    <div class="help-info">Numbers only</div>
-                                </div>
-                                <div class="form-group form-float">
-                                    <div class="form-line">
-                                        <input type="number" class="form-control" name="item_ID" id="itemID">
-                                        <label class="form-label">Item ID</label>
-                                    </div>
-                                    <div class="help-info">Numbers only. If id already in DB then item info will be updated</div>
-                                </div>
-                                
-                                <div class="box form-group form-float">
-					<input type="file" name="file-1[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" required aria-required="true">
-					<label for="file-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> <span>Choose a file…</span></label>
-				</div>
-                              
-                                    
-                                        <!--<div class="dz-message">
-                                            <div class="drag-icon-cph">
-                                                <i class="material-icons">touch_app</i>
-                                            </div>
-                                            <h3>Drop files here or click to upload.</h3>
+                            <div class="body">
+                                <form id="form_advanced_validation" class="js-ajax-php-json" action="#" method="POST"  enctype="multipart/form-data" novalidate="novalidate">
+                                    <div class="form-group form-float">
+                                        <div class="form-line">
+                                            <input type="text" class="form-control" name="name" required aria-required="true" id="item_n">
+                                            <label class="form-label">Item Name</label>
                                         </div>
--->
-                       
+                                    </div>
+                                    <div class="form-group form-float">
+                                        <div class="form-line">
+                                            <textarea name="description_s" cols="30" rows="5" class="form-control no-resize" required aria-required="true"></textarea>
+                                            <label class="form-label">Short Description</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-float">
+                                        <div class="form-line">
+                                            <textarea name="description_f" cols="30" rows="5" class="form-control" required aria-required="true"></textarea>
+                                            <label class="form-label">Full Description</label>
+                                        </div>
+                                    </div>
+                                    <div class="form-group form-float">
+                                        <div class="form-line">
+                                            <input type="number" class="form-control" name="warehouse" required aria-required="true">
+                                            <label class="form-label">Quantity available in warehouse</label>
+                                        </div>
+                                        <div class="help-info">Numbers only</div>
+                                    </div>
+                                    <div class="form-group form-float">
+                                        <div class="form-line">
+                                            <input type="number" class="form-control" name="price" required aria-required="true">
+                                            <label class="form-label">Price</label>
+                                        </div>
+                                        <div class="help-info">Numbers only</div>
+                                    </div>
+                                    <div class="form-group form-float">
+                                        <div class="form-line">
+                                            <input type="number" class="form-control" name="item_ID" id="itemID">
+                                            <label class="form-label">Item ID</label>
+                                        </div>
+                                        <div class="help-info">Numbers only. If id already in DB then item info will be updated</div>
+                                    </div>
 
-                            <input class="btn btn-primary waves-effect" name="submit" type="submit" value="Submit"><!--SUBMIT</button>-->
-                            </form>
+                                    <div class="box form-group form-float">
+                        <input type="file" name="file-1[]" id="file-1" class="inputfile inputfile-1" data-multiple-caption="{count} files selected" required aria-required="true">
+                        <label for="file-1"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="17" viewBox="0 0 20 17"><path d="M10 0l-5.2 4.9h3.3v5.1h3.8v-5.1h3.3l-5.2-4.9zm9.3 11.5l-3.2-2.1h-2l3.4 2.6h-3.5c-.1 0-.2.1-.2.1l-.8 2.3h-6l-.8-2.2c-.1-.1-.1-.2-.2-.2h-3.6l3.4-2.6h-2l-3.2 2.1c-.4.3-.7 1-.6 1.5l.6 3.1c.1.5.7.9 1.2.9h16.3c.6 0 1.1-.4 1.3-.9l.6-3.1c.1-.5-.2-1.2-.7-1.5z"></path></svg> <span>Choose a file…</span></label>
+                    </div>
+
+
+                                            <!--<div class="dz-message">
+                                                <div class="drag-icon-cph">
+                                                    <i class="material-icons">touch_app</i>
+                                                </div>
+                                                <h3>Drop files here or click to upload.</h3>
+                                            </div>
+    -->
+
+
+                                <input class="btn btn-primary waves-effect" name="submit" type="submit" value="Submit"><!--SUBMIT</button>-->
+                                </form>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <? include "table.php"; ?>
+            <? 
+            include "tableData1.php";
+            ?>
+          </div>  
+        
     </section>
     
 
@@ -152,11 +161,23 @@
 
     <!-- Waves Effect Plugin Js -->
     <script src="plugins/node-waves/waves.js"></script>
+    
+    <!-- Jquery DataTable Plugin Js -->
+    <script src="plugins/jquery-datatable/jquery.dataTables.js"></script>
+    <script src="plugins/jquery-datatable/skin/bootstrap/js/dataTables.bootstrap.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/dataTables.buttons.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/buttons.flash.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/jszip.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/pdfmake.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/vfs_fonts.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/buttons.html5.min.js"></script>
+    <script src="plugins/jquery-datatable/extensions/export/buttons.print.min.js"></script>
 
     <!-- Custom Js -->
     
     <script src="js/admin.js"></script>
     <script src="js/pages/forms/form-validation.js"></script>
+    <script src="js/pages/tables/jquery-datatable.js"></script>
    
     
    
