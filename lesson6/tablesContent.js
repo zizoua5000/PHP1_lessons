39c1d9d9c63009e2c25ef05e331e511c7fd76029
zizoua5000 <zizoua5000@gmail.com>
2018-05-15 17:59:30 +0400

    update

--- lesson6/tablesContent.js
+++ lesson6/tablesContent.js
@@ -1,11 +1,11 @@
 $("document").ready(function(){
 $('.dataTable').dataTable( {
-  ajax: {
-    url: "update.json",
-    type: "POST",
-//    dataType: "json",    
-    data: "action=dataTable",
-/*    success: function(data) {
+  "ajax": {
+    "url": "update.php",
+    "type": "POST",
+//    "dataType": "json",    
+    "data": "action=dataTable",
+    /*success: function(data) {
                 console.log("superrr!!!"+data);
   }*/
 }
